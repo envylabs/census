27fafa4b62988644b7cd58effa7009e0f43cdd3c
Mark Kendall <mark@binarylotus.com>
2010-04-16 13:47:48 -0400

    Added documentation comments to the config file

--- generators/census/templates/census.rb
+++ generators/census/templates/census.rb
@@ -6,6 +6,17 @@ Census.configure do |config|
   # config.admin_role = 'current_user.admin?'
 end
 
+#
+# Define data types. All data is stored in the database as strings, and you can
+# provide procs that will be used to convert from the stored strings to any data
+# type you need.
+#
+# :sql_transform - should return a SQL fragment that will be used for comparing
+#                  data when doing search queries
+# :format_data   - is used to convert data from its string representation
+# :validate_data - is used to validate form field submissions, and should return
+#                  nil if the data is valid and an error message if it is invalid
+#
 Census::DataType.define("String")
 
 Census::DataType.define(
@@ -13,10 +24,4 @@ Census::DataType.define(
   :sql_transform  => lambda {|column_name| "CAST(#{column_name} AS SIGNED INTEGER)"},
   :format_data    => lambda {|data| data.to_i unless data.blank? },
   :validate_data  => lambda {|data| "must be a number" unless data =~ /^\d*$/}
-)
-
-Census::DataType.define(
-  "Yes/No",
-  :sql_transform  => lambda {|column_name| "CAST(#{column_name} AS CHAR)"},
-  :format_data    => lambda {|data| %w(1 T t Y y).include?(data) unless data.blank? }
 )
